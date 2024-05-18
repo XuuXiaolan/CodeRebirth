97f4693b6684025246fcae870b4222435e37b2d3
XuuXiao <ammaroaladel@Gmail.com>
2024-05-18 06:56:42 +0100

    github being weird + meteor shower changes

--- Plugin/Thunderstore/README.md
+++
@@ -1,4 +0,0 @@
-# CodeRebirth
-
-Beta-Release ish of code rebirth
-adds a wallet + coin shop item and map object combo, and also a uhh new custom weather with its own scrap, yippee.
--- Plugin/src/CodeRebirthInputs.cs
+++
@@ -1,8 +0,0 @@
-using LethalCompanyInputUtils.Api;
-using UnityEngine.InputSystem;
-
-namespace CodeRebirth.Keybinds;
-public class IngameKeybinds : LcInputActions {
-    [InputAction("<Keyboard>/p", Name = "ExampleKeybind")]
-    public InputAction Example { get; set; }
-}
