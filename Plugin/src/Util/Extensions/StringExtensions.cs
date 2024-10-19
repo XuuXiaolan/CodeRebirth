fa9bc8beeb96e8702e4593bc5526b1722e7c4517
LoafOrc <33192068+LoafOrc@users.noreply.github.com>
2024-10-19 14:45:07 +1000

    util functions/more logging/patch fix for potentially failing to sync data

---
+++ Plugin/src/Util/Extensions/StringExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace CodeRebirth.Util.Extensions;
+
+static class StringExtensions {
+	public static string OrIfEmpty(this string? self, string defaultValue)
+	{
+		return !string.IsNullOrEmpty(self) ? self : defaultValue;
+	}
+}
