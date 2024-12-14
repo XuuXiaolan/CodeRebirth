ba94b61f3f996e44864cd991143ae8177f0ba9ec
XuuXiao <ammaroaladel@Gmail.com>
2024-12-14 02:37:05 +0000

    fixed navmesh in company not having right plugininfo check

--- Plugin/CodeRebirth/src/ModCompats/NavmeshInCompanyCompat.cs
+++ Plugin/CodeRebirth/src/ModCompats/NavmeshInCompanyCompat.cs
@@ -1,5 +1,5 @@
 namespace CodeRebirth.src.ModCompats;
 public static class NavmeshInCompanyCompat
 {
-    public static bool Enabled { get { return BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey("Kittenji.NavMeshInCompany"); } }
+    public static bool Enabled { get { return BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey("dev.kittenji.NavMeshInCompany"); } }
 }
