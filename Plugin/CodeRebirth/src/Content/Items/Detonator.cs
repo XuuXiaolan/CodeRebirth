2a2b1888c2c874d799b57b22d06018a5610de9bd
XuuXiao <ammaroaladel@Gmail.com>
2025-02-23 03:15:20 +0000

    nitro crate + detonator

---
+++ Plugin/CodeRebirth/src/Content/Items/Detonator.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using UnityEngine;
+
+namespace CodeRebirth.src.Content.Items;
+public class Detonator : GrabbableObject
+{
+    public SkinnedMeshRenderer skinnedMeshRenderer = null!;
+
+    public override void InteractItem()
+    {
+        base.InteractItem();
+        StartCoroutine(DoBlowAnimation());
+    }
+
+    private IEnumerator DoBlowAnimation()
+    {
+        float currentWeight = skinnedMeshRenderer.GetBlendShapeWeight(0);
+        while (currentWeight <= 100)
+        {
+            currentWeight = skinnedMeshRenderer.GetBlendShapeWeight(0);
+            skinnedMeshRenderer.SetBlendShapeWeight(0, Mathf.Clamp(currentWeight + Time.deltaTime * 100f, 0, 100f));
+            yield return null;
+        }
+
+        foreach (var crate in NitroCrate.nitroCrates)
+        {
+            crate.RequestServerToDespawnServerRpc();
+        }
+
+        yield return new WaitForSeconds(4f);
+
+        while (currentWeight >= 0)
+        {
+            currentWeight = skinnedMeshRenderer.GetBlendShapeWeight(0);
+            skinnedMeshRenderer.SetBlendShapeWeight(0, Mathf.Clamp(currentWeight - Time.deltaTime * 100f, 0, 100f));
+            yield return null;
+        }
+    }
+}
---
+++ Plugin/CodeRebirth/src/Content/Items/NitroCrate.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using CodeRebirth.src.MiscScripts;
+using Unity.Netcode;
+
+namespace CodeRebirth.src.Content.Items;
+public class NitroCrate : GrabbableObject
+{
+    public static List<NitroCrate> nitroCrates = new();
+
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+        nitroCrates.Add(this);
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        base.OnNetworkDespawn();
+        CRUtilities.CreateExplosion(this.transform.position, true, 999, 0, 6, 100, null, null, 100f);
+        nitroCrates.Remove(this);
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    public void RequestServerToDespawnServerRpc()
+    {
+        playerHeldBy?.DropAllHeldItems();
+        playerHeldBy?.DropAllHeldItemsClientRpc();
+        this.NetworkObject.Despawn();
+    }
+}
