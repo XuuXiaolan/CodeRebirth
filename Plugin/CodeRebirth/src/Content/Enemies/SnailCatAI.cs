003af862ebbf5c5834b10be22112bb5bcf29671a
XuuXiao <ammaroaladel@Gmail.com>
2025-02-24 22:34:58 +0000

    cabin stuff with guardsman start on snailcat scripts

--- Plugin/CodeRebirth/src/Content/Enemies/SnailCatAI.cs
+++ Plugin/CodeRebirth/src/Content/Enemies/SnailCatAI.cs
@@ -1,6 +1,20 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using GameNetcodeStuff;
+using UnityEngine;
+using UnityEngine.AI;
+
 namespace CodeRebirth.src.Content.Enemies;
 public class SnailCatAI : CodeRebirthEnemyAI
 {
+    public SnailCatPhysicsProp propScript = null!;
+    public string[] randomizedNames = [];
+
+    private bool holdingBaby = false;
+    private Coroutine? dropBabyCoroutine = null;
+    private PlayerControllerB? playerHolding = null;
     public enum State
     {
         Wandering,
@@ -25,4 +39,82 @@ public class SnailCatAI : CodeRebirthEnemyAI
                 break;
         }
     }
+
+    public void PickUpBabyLocalClient()
+    {
+		currentOwnershipOnThisClient = (int)propScript.playerHeldBy.playerClientId;
+		inSpecialAnimation = true;
+		agent.enabled = false;
+		holdingBaby = true;
+		if (dropBabyCoroutine != null)
+		{
+			StopCoroutine(dropBabyCoroutine);
+		}
+
+		playerHolding = propScript.playerHeldBy;
+    }
+
+    public void DropBabyLocalClient()
+    {
+		holdingBaby = false;
+		playerHolding = null;
+		if (IsOwner)
+		{
+			ChangeOwnershipOfEnemy(StartOfRound.Instance.allPlayerScripts[0].actualClientId);
+		}
+
+		bool gotValidDropPosition = true;
+		Vector3 itemFloorPosition = propScript.GetItemFloorPosition(propScript.previousPlayerHeldBy.transform.position + Vector3.up * 0.5f);
+		Vector3 groundNavmeshPosition = RoundManager.Instance.GetNavMeshPosition(itemFloorPosition, default(NavMeshHit), 10f, -1);
+
+		if (!RoundManager.Instance.GotNavMeshPositionResult)
+		{
+			gotValidDropPosition = false;
+			itemFloorPosition = propScript.startFallingPosition;
+			if (propScript.transform.parent != null)
+			{
+				itemFloorPosition = propScript.transform.parent.TransformPoint(propScript.startFallingPosition);
+			}
+			Transform transform = ChooseClosestNodeToPositionNoPathCheck(itemFloorPosition);
+			groundNavmeshPosition = RoundManager.Instance.GetNavMeshPosition(transform.transform.position, default(NavMeshHit), 5f, -1);
+		}
+
+		if (propScript.transform.parent == null)
+		{
+			propScript.targetFloorPosition = groundNavmeshPosition;
+		}
+		else
+		{
+			propScript.targetFloorPosition = propScript.transform.parent.InverseTransformPoint(groundNavmeshPosition);
+		}
+
+		if (gotValidDropPosition)
+		{
+			if (dropBabyCoroutine != null)
+			{
+				StopCoroutine(dropBabyCoroutine);
+			}
+			dropBabyCoroutine = StartCoroutine(DropBabyAnimation(groundNavmeshPosition));
+			return;
+		}
+
+		transform.position = groundNavmeshPosition;
+		inSpecialAnimation = false;
+    }
+
+	private IEnumerator DropBabyAnimation(Vector3 dropOnPosition)
+	{
+		float time = Time.realtimeSinceStartup;
+		yield return new WaitUntil(() => propScript.reachedFloorTarget || Time.realtimeSinceStartup - time > 2f);
+        transform.position = dropOnPosition;
+        inSpecialAnimation = false;
+		dropBabyCoroutine = null;
+	}
+
+	public Transform ChooseClosestNodeToPositionNoPathCheck(Vector3 pos)
+	{
+        IEnumerable<GameObject> allAINodes = RoundManager.Instance.insideAINodes.Concat(RoundManager.Instance.outsideAINodes);
+		var nodesTempArray = allAINodes.OrderBy(x => Vector3.Distance(pos, x.transform.position));
+		return nodesTempArray.First().transform;
+	}
 }
---
+++ Plugin/CodeRebirth/src/Content/Enemies/SnailCatPhysicsProp.cs
@@ -0,0 +1,162 @@
+using GameNetcodeStuff;
+using UnityEngine;
+
+namespace CodeRebirth.src.Content.Enemies;
+public class SnailCatPhysicsProp : GrabbableObject
+{
+	public override void EquipItem()
+	{
+		base.EquipItem();
+        snailCatScript.PickUpBabyLocalClient();
+        previousPlayerHeldBy = playerHeldBy;
+	}
+
+	public override void FallWithCurve()
+	{
+		if (snailCatScript.inSpecialAnimation)
+		{
+			base.FallWithCurve();
+		}
+	}
+
+	public override void Start()
+	{
+        propColliders = gameObject.GetComponentsInChildren<Collider>();
+		for (int i = 0; i < propColliders.Length; i++)
+		{
+			if (!propColliders[i].CompareTag("DoNotSet") && !propColliders[i].CompareTag("Enemy"))
+			{
+                propColliders[i].excludeLayers = -2621449;
+			}
+		}
+        originalScale = transform.localScale;
+		if (itemProperties.itemSpawnsOnGround)
+		{
+            startFallingPosition = transform.position;
+			if (transform.parent != null)
+			{
+                startFallingPosition = transform.parent.InverseTransformPoint(startFallingPosition);
+			}
+			FallToGround(false);
+		}
+		else
+		{
+            fallTime = 1f;
+            hasHitGround = true;
+            reachedFloorTarget = true;
+            targetFloorPosition = transform.localPosition;
+		}
+		if (itemProperties.isScrap)
+		{
+            fallTime = 1f;
+            hasHitGround = true;
+		}
+		if (itemProperties.isScrap && RoundManager.Instance.mapPropsContainer != null)
+		{
+            radarIcon = GameObject.Instantiate(StartOfRound.Instance.itemRadarIconPrefab, RoundManager.Instance.mapPropsContainer.transform).transform;
+		}
+		if (!itemProperties.isScrap)
+		{
+			HoarderBugAI.grabbableObjectsInMap.Add(gameObject);
+		}
+		MeshRenderer[] componentsInChildren = gameObject.GetComponentsInChildren<MeshRenderer>();
+		for (int j = 0; j < componentsInChildren.Length; j++)
+		{
+			componentsInChildren[j].renderingLayerMask = 1U;
+		}
+		SkinnedMeshRenderer[] componentsInChildren2 = gameObject.GetComponentsInChildren<SkinnedMeshRenderer>();
+		for (int k = 0; k < componentsInChildren2.Length; k++)
+		{
+			componentsInChildren2[k].renderingLayerMask = 1U;
+		}
+	}
+
+	public override void Update()
+	{
+		if (!isHeld && parentObject == null)
+		{
+			if (fallTime < 1f)
+		    {
+                reachedFloorTarget = false;
+                FallWithCurve();
+				if (transform.localPosition.y - targetFloorPosition.y < 0.05f && !hasHitGround)
+				{
+                    PlayDropSFX();
+                    OnHitGround();
+					return;
+				}
+			}
+			else
+			{
+				if (!reachedFloorTarget)
+				{
+					if (!hasHitGround)
+					{
+                        PlayDropSFX();
+                        OnHitGround();
+					}
+                    reachedFloorTarget = true;
+					if (floorYRot == -1)
+					{
+						transform.rotation = Quaternion.Euler(itemProperties.restingRotation.x, transform.eulerAngles.y, itemProperties.restingRotation.z);
+					}
+					else
+					{
+						transform.rotation = Quaternion.Euler(itemProperties.restingRotation.x, floorYRot + itemProperties.floorYOffset + 90f, itemProperties.restingRotation.z);
+					}
+				}
+				if (snailCatScript.inSpecialAnimation)
+				{
+					transform.localPosition = targetFloorPosition;
+					return;
+				}
+			}
+		}
+		else if (isHeld || isHeldByEnemy)
+		{
+            reachedFloorTarget = false;
+		}
+	}
+
+	public override void LateUpdate()
+	{
+		if (snailCatScript.inSpecialAnimation && parentObject != null)
+		{
+			transform.rotation = parentObject.rotation;
+			transform.Rotate(itemProperties.rotationOffset);
+			transform.position = parentObject.position;
+			Vector3 vector = itemProperties.positionOffset;
+			vector = parentObject.rotation * vector;
+			transform.position += vector;
+		}
+	}
+
+	public override void EnableItemMeshes(bool enable)
+	{
+		MeshRenderer[] componentsInChildren = gameObject.GetComponentsInChildren<MeshRenderer>();
+		for (int i = 0; i < componentsInChildren.Length; i++)
+		{
+			if (!componentsInChildren[i].gameObject.CompareTag("DoNotSet") && !componentsInChildren[i].gameObject.CompareTag("InteractTrigger") && !componentsInChildren[i].gameObject.CompareTag("Enemy"))
+			{
+				componentsInChildren[i].enabled = enable;
+			}
+		}
+		SkinnedMeshRenderer[] componentsInChildren2 = gameObject.GetComponentsInChildren<SkinnedMeshRenderer>();
+		for (int j = 0; j < componentsInChildren2.Length; j++)
+		{
+			componentsInChildren2[j].enabled = enable;
+		}
+	}
+
+	public override void DiscardItem()
+	{		
+        snailCatScript.DropBabyLocalClient();
+		
+        previousPlayerHeldBy = 
+        playerHeldBy;
+		base.DiscardItem();
+	}
+
+	public SnailCatAI snailCatScript;
+	public PlayerControllerB previousPlayerHeldBy;
+}
