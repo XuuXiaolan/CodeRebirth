5eae90fb8b620bf00c8a7f4030d8998247692cd4
XuuXiao <ammaroaladel@Gmail.com>
2025-02-26 17:40:01 +0000

    DID NOT fix loading issue with data. infinikey might be working.

--- Plugin/CodeRebirth/src/Content/Items/InfiniKey.cs
+++ Plugin/CodeRebirth/src/Content/Items/InfiniKey.cs
@@ -1,8 +1,64 @@
+using System;
+using CodeRebirth.src.Util;
+using GameNetcodeStuff;
 using UnityEngine;
 
 namespace CodeRebirth.src.Content.Items;
-public class InfiniKey : KnifeItem
+public class InfiniKey : GrabbableObject
 {
+    private RaycastHit[] cachedHits = new RaycastHit[10];
+    private PlayerControllerB? previousPlayerHeldBy = null;
+    public override void ItemActivate(bool used, bool buttonDown = true)
+    {
+        base.ItemActivate(used, buttonDown);
+        if (this.playerHeldBy != null)
+		{
+			this.previousPlayerHeldBy = this.playerHeldBy;
+			if (this.playerHeldBy.IsOwner)
+			{
+				this.playerHeldBy.playerBodyAnimator.SetTrigger("UseHeldItem1");
+			}
+		}
+		if (IsOwner)
+		{
+			UseInfiniKey();
+		}
+    }
+
+    public void UseInfiniKey()
+    {
+        if (this.previousPlayerHeldBy == null)
+		{
+			Plugin.Logger.LogError("Previousplayerheldby is null on this client when HitShovel is called.");
+			return;
+		}
+		previousPlayerHeldBy.activatingItem = false;
+		int hitSurfaceIndex = -1;
+        this.previousPlayerHeldBy.twoHanded = false;
+        int numHits = Physics.SphereCastNonAlloc(previousPlayerHeldBy.gameplayCamera.transform.position + previousPlayerHeldBy.gameplayCamera.transform.right * 0.1f, 0.3f, previousPlayerHeldBy.gameplayCamera.transform.forward, cachedHits, 0.75f, CodeRebirthUtils.Instance.collidersAndRoomAndRailingAndInteractableMask, QueryTriggerInteraction.Collide);
+        for (int i = 0; i < numHits; i++)
+        {
+            if (cachedHits[i].transform.gameObject.layer == 8 || cachedHits[i].transform.gameObject.layer == 11)
+            {
+                string tag = cachedHits[i].collider.gameObject.tag;
+                hitSurfaceIndex = Array.IndexOf(StartOfRound.Instance.footstepSurfaces, tag);
+                return;
+            }
+            OnHit(cachedHits[i].collider);
+        }
+		if (hitSurfaceIndex != -1)
+		{
+			/*RoundManager.PlayRandomClip(this.knifeAudio, this.hitSFX, true, 1f, 0, 1000);
+			RoundManager.Instance.PlayAudibleNoise(base.transform.position, 17f, 0.8f, 0, false, 0);
+			if (hitSurfaceIndex != -1)
+			{
+				this.knifeAudio.PlayOneShot(StartOfRound.Instance.footstepSurfaces[hitSurfaceIndex].hitSurfaceSFX);
+				WalkieTalkie.TransmitOneShotAudio(this.knifeAudio, StartOfRound.Instance.footstepSurfaces[hitSurfaceIndex].hitSurfaceSFX, 1f);
+			}
+			this.HitShovelServerRpc(hitSurfaceIndex);*/
+		}
+    }
+
     public void OnHit(Collider collider)
     {
         if (collider == null) return;
--- Plugin/CodeRebirth/src/Patches/KnifeItemPatch.cs
+++ Plugin/CodeRebirth/src/Patches/KnifeItemPatch.cs
@@ -10,17 +10,11 @@ public static class KnifeItemPatch
 
     public static void Init()
     {
-        On.KnifeItem.HitKnife += KnifeItem_HitKnife;
+        // On.KnifeItem.HitKnife += KnifeItem_HitKnife;
     }
 
     private static void KnifeItem_HitKnife(On.KnifeItem.orig_HitKnife orig, KnifeItem self, bool cancel)
     {
         orig(self, cancel);
-        if (cancel || self is not InfiniKey infiniKey) return;
-        int numHits = Physics.OverlapSphereNonAlloc(self.transform.position, 2f, cachedColliders, CodeRebirthUtils.Instance.interactableMask, QueryTriggerInteraction.Collide);
-        for (int i = 0; i < numHits; i++)
-        {
-            infiniKey.OnHit(cachedColliders[i]);
-        }
     }
 }
