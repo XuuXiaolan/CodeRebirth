345c7948d802fb7e0bdb99694363f6894dd1a7b5
XuXiaolan <ammaroaladel@gmail.com>
2025-03-06 21:22:58 +0000

    nitro crate explodes from other objects hitting it now

--- Plugin/CodeRebirth/src/Content/Items/NitroCrate.cs
+++ Plugin/CodeRebirth/src/Content/Items/NitroCrate.cs
@@ -1,9 +1,11 @@
 using System.Collections.Generic;
 using CodeRebirth.src.MiscScripts;
+using GameNetcodeStuff;
 using Unity.Netcode;
+using UnityEngine;
 
 namespace CodeRebirth.src.Content.Items;
-public class NitroCrate : GrabbableObject
+public class NitroCrate : GrabbableObject, IHittable
 {
     public static List<NitroCrate> nitroCrates = new();
 
@@ -26,5 +28,11 @@ public class NitroCrate : GrabbableObject
         playerHeldBy?.DropAllHeldItems();
         playerHeldBy?.DropAllHeldItemsClientRpc();
         this.NetworkObject.Despawn();
+    }
+
+    public bool Hit(int force, Vector3 hitDirection, PlayerControllerB playerWhoHit = null, bool playHitSFX = false, int hitID = -1)
+    {
+        RequestServerToDespawnServerRpc();
+        return true;
     }
 }
