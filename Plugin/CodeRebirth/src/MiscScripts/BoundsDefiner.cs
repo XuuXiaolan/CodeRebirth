336c731ff6073164ea6ff9a73f2d102376025fa8
XuuXiao <ammaroaladel@Gmail.com>
2025-03-16 02:42:53 +0000

    fixed bounds definer not checking proper contain bounds. fixed quicksand with dropship, items dont stick to dropship though

--- Plugin/CodeRebirth/src/MiscScripts/BoundsDefiner.cs
+++ Plugin/CodeRebirth/src/MiscScripts/BoundsDefiner.cs
@@ -9,22 +9,15 @@ public class BoundsDefiner : MonoBehaviour
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = boundColor;
-
-        // Save the original matrix so we can restore it later.
         Matrix4x4 oldGizmosMatrix = Gizmos.matrix;
-        
-        // Set the Gizmos matrix to include the GameObject's position and rotation.
         Gizmos.matrix = Matrix4x4.TRS(transform.position, transform.rotation, Vector3.one);
-        
-        // Now draw the cube in local space.
         Gizmos.DrawWireCube(bounds.center, bounds.size);
-        
-        // Restore the original Gizmos matrix.
         Gizmos.matrix = oldGizmosMatrix;
     }
 
-    public bool BoundsContainTransform(Transform transform)
+    public bool BoundsContainTransform(Transform target)
     {
-        return new Bounds(transform.position + bounds.center, bounds.size).Contains(transform.position);
+        Vector3 localPos = transform.InverseTransformPoint(target.position);
+        return bounds.Contains(localPos);
     }
 }
