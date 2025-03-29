4529d9c349af822d5baa6de8984984aea239efb5
XuuXiao <ammaroaladel@Gmail.com>
2025-03-29 13:37:18 +0000

    started some work on mountaineer

--- Plugin/CodeRebirth/src/Content/Items/.ItemHandler.cs
+++ Plugin/CodeRebirth/src/Content/Items/.ItemHandler.cs
@@ -9,28 +9,8 @@ public class ItemHandler : ContentHandler<ItemHandler>
     {
     }
 
-    public class EpicAxeAssets(string bundleName) : AssetBundleLoader<EpicAxeAssets>(bundleName)
+    public class BrrreakerAssets(string bundleName) : AssetBundleLoader<BrrreakerAssets>(bundleName)
     {
-        [LoadFromBundle("EpicAxeObj.asset")]
-        public Item EpicAxeItem { get; private set; } = null!;
-    }
-
-    public class NaturesMaceAssets(string bundleName) : AssetBundleLoader<NaturesMaceAssets>(bundleName)
-    {
-        [LoadFromBundle("NaturesMaceObj.asset")]
-        public Item NatureMaceItem { get; private set; } = null!;
-    }
-
-    public class IcyHammerAssets(string bundleName) : AssetBundleLoader<IcyHammerAssets>(bundleName)
-    {
-        [LoadFromBundle("IcyHammerObj.asset")]
-        public Item IcyHammerItem { get; private set; } = null!;
-    }
-
-    public class SpikyMaceAssets(string bundleName) : AssetBundleLoader<SpikyMaceAssets>(bundleName)
-    {
-        [LoadFromBundle("SpikyMaceObj.asset")]
-        public Item SpikyMaceItem { get; private set; } = null!;
     }
 
     public class SnowGlobeAssets(string bundleName) : AssetBundleLoader<SnowGlobeAssets>(bundleName)
@@ -50,11 +30,8 @@ public class ItemHandler : ContentHandler<ItemHandler>
     public XuAndRigoAssets? XuAndRigo { get; private set; } = null;
     public ZortAssets? Zort { get; private set; } = null;
     public HoverboardAssets? Hoverboard { get; private set; } = null;
-    public EpicAxeAssets? EpicAxe { get; private set; } = null;
     public SnowGlobeAssets? SnowGlobe { get; private set; } = null;
-    public NaturesMaceAssets? NaturesMace { get; private set; } = null;
-    public IcyHammerAssets? IcyHammer { get; private set; } = null;
-    public SpikyMaceAssets? SpikyMace { get; private set; } = null;
+    public BrrreakerAssets? Brrreaker { get; private set; } = null;
 
     public ItemHandler()
     {
@@ -67,28 +44,6 @@ public class ItemHandler : ContentHandler<ItemHandler>
 
         SnowGlobe = LoadAndRegisterAssets<SnowGlobeAssets>("snowglobeassets");
 
-        /*if (Plugin.ModConfig.ConfigEpicAxeScrapEnabled.Value)
-        {
-            EpicAxe = new EpicAxeAssets("epicaxeassets");
-            RegisterShopItemWithConfig(false, true, EpicAxe.EpicAxeItem, null, 0, Plugin.ModConfig.ConfigEpicAxeScrapSpawnWeights.Value, Plugin.ModConfig.ConfigEpicAxeWorth.Value);
-        }
-
-        if (Plugin.ModConfig.ConfigNaturesMaceScrapEnabled.Value)
-        {
-            NaturesMace = new NaturesMaceAssets("naturesmaceassets");
-            RegisterShopItemWithConfig(false, true, NaturesMace.NatureMaceItem, null, 0, Plugin.ModConfig.ConfigNaturesMaceScrapSpawnWeights.Value, Plugin.ModConfig.ConfigNaturesMaceWorth.Value);
-        }
-
-        if (Plugin.ModConfig.ConfigIcyHammerScrapEnabled.Value)
-        {
-            IcyHammer = new IcyHammerAssets("icyhammerassets");
-            RegisterShopItemWithConfig(false, true, IcyHammer.IcyHammerItem, null, 0, Plugin.ModConfig.ConfigIcyHammerScrapSpawnWeights.Value, Plugin.ModConfig.ConfigIcyHammerWorth.Value);
-        }
-
-        if (Plugin.ModConfig.ConfigSpikyMaceScrapEnabled.Value)
-        {
-            SpikyMace = new SpikyMaceAssets("spikymaceassets");
-            RegisterShopItemWithConfig(false, true, SpikyMace.SpikyMaceItem, null, 0, Plugin.ModConfig.ConfigSpikyMaceScrapSpawnWeights.Value, Plugin.ModConfig.ConfigSpikyMaceWorth.Value);
-        }*/
+        Brrreaker = LoadAndRegisterAssets<BrrreakerAssets>("mountaineerassets");
     }
 }
--- Plugin/CodeRebirth/src/Content/Items/Mountaineer.cs
+++ Plugin/CodeRebirth/src/Content/Items/Mountaineer.cs
@@ -1,14 +1,96 @@
+using GameNetcodeStuff;
+using UnityEngine;
+
 namespace CodeRebirth.src.Content.Items;
 public class Mountaineer : GrabbableObject
 {
+    private bool stuckToWall = false;
+    private PlayerControllerB? stuckPlayer = null;
+
+    public override void Update()
+    {
+        base.Update();
+
+        if (stuckPlayer != null && stuckToWall)
+        {
+            stuckPlayer.ResetFallGravity();
+            stuckPlayer.disableMoveInput = true;
+            stuckPlayer.activatingItem = true;
+            // stuckPlayer.grab
+            if (stuckPlayer.jumpCoroutine != null) // this doesn't work when the player lets go of the mouse to discard the item in the middle of a jump lol, find a better way to detect jump start.
+            {
+                stuckPlayer.activatingItem = false;
+                stuckPlayer.disableMoveInput = false;
+                if (stuckPlayer == GameNetworkManager.Instance.localPlayerController) MakePlayerGrabObject(stuckPlayer);
+                stuckPlayer = null;
+                stuckToWall = false;
+            }
+        }
+    }
+
+    private void MakePlayerGrabObject(PlayerControllerB player)
+    {
+        player.currentlyGrabbingObject = this;
+        player.currentlyGrabbingObject.InteractItem();
+        if (player.currentlyGrabbingObject.grabbable && player.FirstEmptyItemSlot() != -1)
+        {
+            player.playerBodyAnimator.SetBool("GrabInvalidated", false);
+            player.playerBodyAnimator.SetBool("GrabValidated", false);
+            player.playerBodyAnimator.SetBool("cancelHolding", false);
+            player.playerBodyAnimator.ResetTrigger("Throw");
+            player.SetSpecialGrabAnimationBool(true, null);
+            player.isGrabbingObjectAnimation = true;
+            player.cursorIcon.enabled = false;
+            player.cursorTip.text = "";
+            player.twoHanded = player.currentlyGrabbingObject.itemProperties.twoHanded;
+            player.carryWeight = Mathf.Clamp(player.carryWeight + (player.currentlyGrabbingObject.itemProperties.weight - 1f), 1f, 10f);
+            if (player.currentlyGrabbingObject.itemProperties.grabAnimationTime > 0f)
+            {
+                player.grabObjectAnimationTime = player.currentlyGrabbingObject.itemProperties.grabAnimationTime;
+            }
+            else
+            {
+                player.grabObjectAnimationTime = 0.4f;
+            }
+            if (!player.isTestingPlayer)
+            {
+                player.GrabObjectServerRpc(this.NetworkObject);
+            }
+            if (player.grabObjectCoroutine != null)
+            {
+                base.StopCoroutine(player.grabObjectCoroutine);
+            }
+            player.grabObjectCoroutine = base.StartCoroutine(player.GrabObject());
+        }
+    }
+
     public override void ItemActivate(bool used, bool buttonDown = true)
     {
         base.ItemActivate(used, buttonDown);
 
-        // Hold type mechanic
-        // Enable specialanimation so they cant drop.
+        isBeingUsed = buttonDown;
+        playerHeldBy.activatingItem = buttonDown;
+
+        if (!buttonDown)
+        {
+            stuckPlayer = playerHeldBy;
+            stuckToWall = true;
+            StartCoroutine(playerHeldBy.waitToEndOfFrameToDiscard());
+        }
         // Also maybe instead override fall curve so that it doesnt fall down if you drop it and just sticks
-        // Constantly resets player gravity on update.
-        // When let go launches the player a bit up
+        // pressing Jump key lets you launch up a bit and regrabs the item.
+    }
+
+    public override void FallWithCurve()
+    {
+        if (stuckToWall) return;
+        base.FallWithCurve();
+    }
+
+    public override void EquipItem()
+    {
+        base.EquipItem();
+        stuckToWall = false;
+        stuckPlayer = null;
     }
 }
