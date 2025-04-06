f452eb55ecc343fdb2b7b0517ff29ee9166ece28
XuuXiao <ammaroaladel@Gmail.com>
2025-04-06 23:03:07 +0100

    made the targetplayerspine thing look a bit nicer. hid the rabbit for the local player on finishing reveal animation. did a bit more to the rabbit. potentially finished the rabbit, needs testing

--- Plugin/CodeRebirth/src/Content/Enemies/RabbitMagician.cs
+++ Plugin/CodeRebirth/src/Content/Enemies/RabbitMagician.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using CodeRebirth.src.Content.Enemies;
 using CodeRebirth.src.Util;
 using GameNetcodeStuff;
+using Unity.Netcode;
 using UnityEngine;
 
 public class RabbitMagician : CodeRebirthEnemyAI
@@ -12,9 +13,13 @@ public class RabbitMagician : CodeRebirthEnemyAI
     [SerializeField]
     private AnimationClip _spottedAnimation = null!;
     [SerializeField]
+    private AnimationClip _latchOnAnimation = null!;
+
+    [SerializeField]
     private Vector3 _offsetPosition = new Vector3(0.031f, -0.109f, -0.471f);
 
     private Coroutine? _attachRoutine = null;
+    private Coroutine? _idleRoutine = null;
     private Transform? _targetPlayerSpine3 = null;
 
     private static readonly int RunSpeedFloat = Animator.StringToHash("RunSpeed"); // Float
@@ -36,7 +41,7 @@ public class RabbitMagician : CodeRebirthEnemyAI
     public override void Start()
     {
         base.Start();
-
+        _offsetPosition = new Vector3(0.031f, -0.109f, -0.471f);
         foreach (var enemyCollider in this.GetComponentsInChildren<Collider>())
         {
             foreach (var playerCollider in GameNetworkManager.Instance.localPlayerController.GetCRPlayerData().playerColliders)
@@ -46,15 +51,23 @@ public class RabbitMagician : CodeRebirthEnemyAI
         }
         SwitchToBehaviourStateOnLocalClient((int)RabbitMagicianState.Spawn);
         if (!IsServer) return;
-        StartCoroutine(SwitchToIdle());
+        _idleRoutine = StartCoroutine(SwitchToIdle());
     }
 
     public override void Update()
     {
         base.Update();
 
-        if (currentBehaviourStateIndex != (int)RabbitMagicianState.Attached || _targetPlayerSpine3 == null) return;
-        // Rotate the offset vector by the pivot's rotation and add it to the pivot's position.
+        if (currentBehaviourStateIndex != (int)RabbitMagicianState.Attached)
+            return;
+
+        if (_attachRoutine != null)
+            return;
+
+        if (targetPlayer == null)
+            return;
+
+        _targetPlayerSpine3 = targetPlayer.upperSpine;
         Vector3 worldOffset = _targetPlayerSpine3.rotation * _offsetPosition;
         this.transform.position = _targetPlayerSpine3.position + worldOffset;
         this.transform.rotation = _targetPlayerSpine3.rotation;
@@ -93,15 +106,10 @@ public class RabbitMagician : CodeRebirthEnemyAI
         creatureAnimator.SetFloat(RunSpeedFloat, agent.velocity.magnitude / 3f);
         foreach (var player in StartOfRound.Instance.allPlayerScripts)
         {
-            if (player.isPlayerDead || !player.isPlayerControlled) continue;
-            Vector3 directionToRabbit = (transform.position - player.gameObject.transform.position).normalized;
-            if (Vector3.Dot(player.gameplayCamera.transform.forward, directionToRabbit) < 0.2f) continue;
-            if (Physics.Linecast(transform.position, player.transform.position, StartOfRound.Instance.collidersAndRoomMaskAndDefault, QueryTriggerInteraction.Ignore)) continue;
-            smartAgentNavigator.StopSearchRoutine();
-            SetTargetServerRpc(Array.IndexOf(StartOfRound.Instance.allPlayerScripts, player));
-            SwitchToBehaviourServerRpc((int)RabbitMagicianState.Attached);
-            smartAgentNavigator.enabled = false;
-            agent.enabled = false;
+            if (player.isPlayerDead || !player.isPlayerControlled)
+                continue;
+            if (!PlayerLookingAtRabbit(player))
+                continue;
             _attachRoutine = StartCoroutine(AttachToPlayer(player));
             // do animation
             // attach to a player bone
@@ -111,7 +119,25 @@ public class RabbitMagician : CodeRebirthEnemyAI
 
     public void DoAttached()
     {
+        if (targetPlayer == null || targetPlayer.isPlayerDead)
+        {
+            if (_idleRoutine != null)
+                return;
 
+            _idleRoutine = StartCoroutine(SwitchToIdle());
+            return;
+        }
+        foreach (var player in StartOfRound.Instance.allPlayerScripts)
+        {
+            if (player.isPlayerDead || player.isPlayerControlled)
+                continue;
+            if (player == targetPlayer)
+                continue;
+            if (!PlayerLookingAtRabbit(player))
+                continue;
+
+            StartCoroutine(KillPlayerAndSwitchTarget(player));
+        }
     }
 
     public void DoSwitchingTarget()
@@ -121,17 +147,49 @@ public class RabbitMagician : CodeRebirthEnemyAI
     #endregion
 
     #region Misc Functions
+
+    private IEnumerator KillPlayerAndSwitchTarget(PlayerControllerB newTargetPlayer)
+    {
+        SwitchToBehaviourServerRpc((int)RabbitMagicianState.SwitchingTarget);
+        yield return StartCoroutine(AttachToPlayer(newTargetPlayer));
+    }
+
+    private bool PlayerLookingAtRabbit(PlayerControllerB player)
+    {
+        Vector3 directionToRabbit = (transform.position - player.gameObject.transform.position).normalized;
+        if (Vector3.Dot(player.gameplayCamera.transform.forward, directionToRabbit) < 0.2f)
+            return false;
+        if (Physics.Linecast(transform.position, player.transform.position, StartOfRound.Instance.collidersAndRoomMaskAndDefault, QueryTriggerInteraction.Ignore))
+            return false;
+        return true;
+    }
+
     private IEnumerator SwitchToIdle()
     {
         yield return new WaitForSeconds(_spawnAnimation.length);
+        smartAgentNavigator.enabled = true;
+        agent.enabled = true;
         SwitchToBehaviourServerRpc((int)RabbitMagicianState.Idle);
         smartAgentNavigator.StartSearchRoutine(transform.position, 20);
+        _idleRoutine = null;
     }
 
     public IEnumerator AttachToPlayer(PlayerControllerB playerToAttachTo)
     {
+        smartAgentNavigator.StopSearchRoutine();
+        SetTargetServerRpc(Array.IndexOf(StartOfRound.Instance.allPlayerScripts, playerToAttachTo));
+        SwitchToBehaviourServerRpc((int)RabbitMagicianState.Attached);
+        smartAgentNavigator.enabled = false;
+        agent.enabled = false;
         creatureNetworkAnimator.SetTrigger(SpottedAnimation);
         yield return new WaitForSeconds(_spottedAnimation.length);
+        if (playerToAttachTo == null || playerToAttachTo.isPlayerDead)
+        {
+            _idleRoutine = StartCoroutine(SwitchToIdle());
+            _attachRoutine = null;
+            yield break;
+        }
+
         bool lookedAt = true;
         while (lookedAt)
         {
@@ -150,10 +208,35 @@ public class RabbitMagician : CodeRebirthEnemyAI
         }
 
         creatureNetworkAnimator.SetTrigger(LatchOnAnimation);
-        _targetPlayerSpine3 = playerToAttachTo.upperSpine;
         this.transform.localScale = new Vector3(0.8915618f, 0.8915618f, 0.8915618f);
         SwitchToBehaviourServerRpc((int)RabbitMagicianState.Attached);
+        StartCoroutine(HideForTargetPlayer());
         _attachRoutine = null;
+    }
+
+    public IEnumerator HideForTargetPlayer()
+    {
+        yield return new WaitForSeconds(_latchOnAnimation.length);
+        if (targetPlayer == null || targetPlayer.isPlayerDead || currentBehaviourStateIndex != (int)RabbitMagicianState.Attached)
+            yield break;
+
+        HideModelForTargetPlayerServerRpc();
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void HideModelForTargetPlayerServerRpc()
+    {
+        HideModelForTargetPlayerClientRpc();
+    }
+
+    [ClientRpc]
+    private void HideModelForTargetPlayerClientRpc()
+    {
+        skinnedMeshRenderers[0].enabled = true;
+        if (GameNetworkManager.Instance.localPlayerController != targetPlayer)
+            return;
+
+        skinnedMeshRenderers[0].enabled = false;
     }
     #endregion
 }
