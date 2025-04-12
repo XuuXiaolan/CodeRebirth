8d6161190a410d3fdcd3ef0da215efff6fdac9b6
XuuXiao <ammaroaladel@Gmail.com>
2025-04-12 06:12:22 +0100

    accidently committed goldrigo meteor test stuff

--- Plugin/CodeRebirth/src/Content/Items/GoldRigo.cs
+++ Plugin/CodeRebirth/src/Content/Items/GoldRigo.cs
@@ -14,18 +14,4 @@ public class GoldRigo : GrabbableObject
         // When player drops the GoldRigo, the SmallRigo's grab it and take it to where it was spawned, and they rest there until deactivated.
         // Depending on what phase the game is in and what they're doing, either their agent is disabled temporarily, or they're hidden.
     }
-
-    public override void EquipItem()
-    {
-        base.EquipItem();
-        if (!IsServer)
-            return;
-
-        GameObject newGameObject = Instantiate(this.gameObject);
-        newGameObject.AddComponent<FallingObjectBehaviour>();
-        Destroy(newGameObject.GetComponent<GoldRigo>());
-        newGameObject.transform.localScale = new Vector3(10f, 10f, 10f);
-        newGameObject.transform.position = new Vector3(1000, 1000, 1000);
-        CodeRebirthUtils.Instance.CreateFallingObject<FallingObjectBehaviour>(newGameObject, playerHeldBy.transform.position + Vector3.up * 250f, playerHeldBy.transform.position, 10f);
-    }
 }
