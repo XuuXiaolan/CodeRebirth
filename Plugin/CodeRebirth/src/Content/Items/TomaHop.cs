ec1a31c930760c7103722365cf5b0f82e9788fa3
XuuXiao <ammaroaladel@Gmail.com>
2025-04-13 02:56:56 +0100

    did some extra thing with tomahop and added todo

--- Plugin/CodeRebirth/src/Content/Items/TomaHop.cs
+++ Plugin/CodeRebirth/src/Content/Items/TomaHop.cs
@@ -17,10 +17,17 @@ public class TomaHop : CRWeapon
 
     public void OnEnemyHitEvent(EnemyAI enemyAI)
     {
-        Plugin.ExtendedLogging($"Player's previous velocity: {previousPlayerHeldBy.velocityLastFrame}");
-        // todo: do more damage based on velocity
-        // todo: decide whether to launch player up based on velocity.
-        // just like the minecraft mace!
-        // previousPlayerHeldBy.externalForceAutoFade += Vector3.up * 20f;
+        // so fall value lower than -10 means the player is falling a sufficient speed,
+        if (previousPlayerHeldBy.fallValue > 0)
+            return;
+
+        int newFallValue = (int)previousPlayerHeldBy.fallValue * -1;
+        previousPlayerHeldBy.ResetFallGravity();
+        previousPlayerHeldBy.externalForces = Vector3.up * newFallValue;
+        previousPlayerHeldBy.externalForceAutoFade += Vector3.up * newFallValue;
+        enemyAI.HitEnemyOnLocalClient(newFallValue / 10, transform.position, previousPlayerHeldBy, true, -1);
+        // todo: press R and send the user up in the air.
+        // todo: when not hitting anything whilst landing from a fall, take reduced fall damage.
+        // todo: if hit an enemy, take no fall damage.
     }
 }
