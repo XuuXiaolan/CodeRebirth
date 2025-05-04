2cf15fb9ca499c1a77993a086a4617555f498e23
XuuXiao <ammaroaladel@Gmail.com>
2025-05-04 06:20:17 +0100

    added change-able screenshake to cacti rising

--- Plugin/CodeRebirth/src/MiscScripts/RiseFromGroundOnSpawn.cs
+++ Plugin/CodeRebirth/src/MiscScripts/RiseFromGroundOnSpawn.cs
@@ -5,11 +5,15 @@ public class RiseFromGroundOnSpawn : MonoBehaviour
 {
     public float depthToRaise = 1f;
     public float raiseSpeed = 0.25f;
+    public ScreenShakeType screenShakeOnRise = ScreenShakeType.Small;
 
     private float _timeToTake = 4f;
     private Vector3 _originalPosition = Vector3.zero;
     public void Start()
     {
+        if (Vector3.Distance(GameNetworkManager.Instance.localPlayerController.transform.position, this.transform.position) <= 5f)
+            HUDManager.Instance.ShakeCamera(screenShakeOnRise);
+
         _originalPosition = this.transform.position;
         this.transform.position = this.transform.position + transform.up * -depthToRaise;
         _timeToTake = depthToRaise / raiseSpeed;
