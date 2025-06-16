7dd19ec49fe80f2de1be9523382fd3b6aad2245c
XuuXiao <ammaroaladel@Gmail.com>
2025-06-16 14:15:48 +0100

    built dll and added material stuff for the lights manager

--- Plugin/CodeRebirth/src/MiscScripts/OxydeLightsManager.cs
+++ Plugin/CodeRebirth/src/MiscScripts/OxydeLightsManager.cs
@@ -6,6 +6,8 @@ public class OxydeLightsManager : MonoBehaviour
 {
     [SerializeField]
     private Color[] _colors = [];
+    [SerializeField]
+    private Renderer[] _renderers = [];
 
     private Light[] _lights = [];
     private int _currentLightIndex = 0;
@@ -14,15 +16,24 @@ public class OxydeLightsManager : MonoBehaviour
     private void Start()
     {
         oxydeLightsManager = this;
+        IncrementLights();
     }
 
     public void IncrementLights()
     {
-        if (_currentLightIndex >= _colors.Length - 1)
+        if (_currentLightIndex >= _colors.Length - 1 || _colors.Length == 0)
             return;
 
-        _currentLightIndex++;
         foreach (var light in _lights)
             light.color = _colors[_currentLightIndex];
+
+        foreach (var renderer in _renderers)
+        {
+            foreach (var sharedMaterial in renderer.sharedMaterials)
+            {
+                sharedMaterial.color = _colors[_currentLightIndex];
+            }
+        }
+        _currentLightIndex++;
     }
 }
