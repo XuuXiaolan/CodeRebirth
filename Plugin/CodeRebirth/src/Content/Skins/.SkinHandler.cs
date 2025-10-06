80b0d3da2d9f6f67b07ba0972f5869a5171542b3
XuuXiao <ammaroaladel@Gmail.com>
2025-10-06 10:22:47 +0100

    added skin handler

---
+++ Plugin/CodeRebirth/src/Content/Skins/.SkinHandler.cs
@@ -0,0 +1,16 @@
+﻿using Dusk;
+
+namespace CodeRebirth.src.Content.Enemies;
+public class SkinHandler : ContentHandler<SkinHandler>
+{
+    public class CRHalloweenAssets(DuskMod mod, string filePath) : AssetBundleLoader<CRHalloweenAssets>(mod, filePath)
+    {
+    }
+
+    public CRHalloweenAssets? CRHalloween = null;
+
+    public SkinHandler(DuskMod mod) : base(mod)
+    {
+        RegisterContent("halloweencrassets", out CRHalloween);
+    }
+}
