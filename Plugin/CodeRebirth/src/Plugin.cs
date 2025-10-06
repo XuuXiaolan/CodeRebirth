81533c6c0c454a854658ae626529f5bc8bfea0d4
XuuXiao <ammaroaladel@Gmail.com>
2025-10-06 10:25:22 +0100

    bwa

--- Plugin/CodeRebirth/src/Plugin.cs
+++ Plugin/CodeRebirth/src/Plugin.cs
@@ -13,6 +13,12 @@ using Dawn;
 
 namespace CodeRebirth.src;
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInDependency("mrov.WeatherRegistry")]
+[BepInDependency("com.rune580.LethalCompanyInputUtils")]
+[BepInDependency(DawnLib.PLUGIN_GUID)]
+[BepInDependency(LethalLevelLoader.Plugin.ModGUID)]
+[BepInDependency(Dusk.MyPluginInfo.PLUGIN_GUID)]
+[BepInDependency("Zaggy1024.OpenBodyCams", BepInDependency.DependencyFlags.SoftDependency)]
 public class Plugin : BaseUnityPlugin
 {
     internal new static ManualLogSource Logger = null!;
@@ -36,7 +42,70 @@ public class Plugin : BaseUnityPlugin
 
     private void Awake()
     {
+        Logger = base.Logger;
+        configFile = this.Config;
+        ModConfig = new CodeRebirthConfig();
 
+        ModConfig.ConfigExtendedLogging = configFile.Bind("Debug Options",
+                                            "Debug Mode | Extended Logging",
+                                            false,
+                                            "Whether ExtendedLogging is enabled.");
+
+        _harmony.PatchAll(typeof(PlayerControllerBPatch));
+        _harmony.PatchAll(typeof(EnemyAIPatch));
+        _harmony.PatchAll(typeof(ShovelPatch));
+        _harmony.PatchAll(typeof(DoorLockPatch));
+        _harmony.PatchAll(typeof(MineshaftElevatorControllerPatch));
+        _harmony.PatchAll(typeof(KeyItemPatch));
+        _harmony.PatchAll(typeof(RoundManagerPatch));
+        _harmony.PatchAll(typeof(StartOfRoundPatch));
+        _harmony.PatchAll(typeof(NetworkBehaviourPatch));
+        _harmony.PatchAll(typeof(HDAdditionalLightDataPatch));
+
+        ItemDropshipPatch.Init();
+        KnifeItemPatch.Init();
+        PlayerControllerBPatch.Init();
+        EnemyAIPatch.Init();
+        ShovelPatch.Init();
+        DoorLockPatch.Init();
+        MineshaftElevatorControllerPatch.Init();
+        SpikeTrapPatch.Init();
+        EnemyAICollisionDetectPatch.Init();
+        LandminePatch.Init();
+        ShotgunItemPatch.Init();
+        TurretPatch.Init();
+        GameNetworkManagerPatch.Init();
+        DeleteFileButtonPatch.Init();
+        SoccerBallPropPatch.Init();
+        // This should be ran before Network Prefabs are registered.
+        InitializeNetworkBehaviours();
+        InputActionsInstance = new IngameKeybinds();
+
+        ModConfig.InitMainCodeRebirthConfig(configFile);
+
+        AssetBundle mainBundle = AssetBundleUtils.LoadBundle(Assembly.GetExecutingAssembly(), "coderebirthasset");
+        Assets = new MainAssets(mainBundle);
+        Mod = DuskMod.RegisterMod(this, mainBundle);
+        Mod.RegisterContentHandlers();
+
+        ModConfig.InitCodeRebirthConfig(configFile);
+
+        Logger.LogInfo("Registering CodeRebirth content.");
+
+        if (OpenBodyCamCompatibilityChecker.Enabled)
+        {
+            OpenBodyCamCompatibilityChecker.Init();
+        }
+
+        if (ModConfig.ConfigCleanUnusedConfigs.Value)
+        {
+            Logger.LogInfo("Cleaning config");
+            Config.ClearUnusedEntries();
+        }
+
+        Config.Save();
+
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
     }
 
     internal static void ExtendedLogging(object text)
