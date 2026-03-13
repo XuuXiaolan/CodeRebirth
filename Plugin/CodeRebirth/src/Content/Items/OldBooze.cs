3e122c78d9c1708cca4289c7d95e021e5053c238
XuuXiao <ammaroaladel@Gmail.com>
2026-03-13 21:37:30 +0000

    oldbooze should be working

--- Plugin/CodeRebirth/src/Content/Items/OldBooze.cs
+++ Plugin/CodeRebirth/src/Content/Items/OldBooze.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using Dawn.Utils;
+using GameNetcodeStuff;
 using UnityEngine;
 
 namespace CodeRebirth.src.Content.Items;
@@ -48,10 +49,29 @@ public class OldBooze : GrabbableObject
 
     private IEnumerator DrinkBooze()
     {
-        yield return new WaitForSeconds(1f);
-        playerHeldBy.DamagePlayer(-50, true, true);
-        playerHeldBy.activatingItem = false;
-        playerHeldBy.DiscardHeldObject();
+        PlayerControllerB playerDrunk = playerHeldBy;
+        float duration = 1f;
+        while (duration > 0)
+        {
+            duration -= Time.deltaTime;
+            playerDrunk.drunknessInertia = Mathf.Clamp(playerDrunk.drunknessInertia + Time.deltaTime * 3f * playerDrunk.drunknessSpeed, 0.1f, 3f);
+            playerDrunk.increasingDrunknessThisFrame = true;
+            yield return null;
+        }
+
+        if (playerDrunk.isPlayerDead)
+        {
+            grabbable = false;
+            customGrabTooltip = "Empty...";
+            if (playerHeldBy != null && playerHeldBy.IsLocalPlayer())
+            {
+                playerHeldBy.DestroyItemInSlotAndSync(Array.IndexOf(playerHeldBy.ItemSlots, this));
+            }
+            yield break;
+        }
+        playerDrunk.DamagePlayer(-50, true, true);
+        playerDrunk.activatingItem = false;
+        playerDrunk.DiscardHeldObject();
         grabbable = false;
         customGrabTooltip = "Empty...";
     }
