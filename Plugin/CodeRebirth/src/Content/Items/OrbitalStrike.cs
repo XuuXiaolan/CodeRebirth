74a94e06185cdc268dd594304da0a3ebeb7f7219
kaliber64 <45779233+kaliber64@users.noreply.github.com>
2026-04-09 02:34:10 -0400

    OrbitalStrike Part 2

--- Plugin/CodeRebirth/src/Content/Items/OrbitalStrike.cs
+++ Plugin/CodeRebirth/src/Content/Items/OrbitalStrike.cs
@@ -7,6 +7,8 @@ using UnityEngine;
 using UnityEngine.VFX;
 using Dawn.Utils;
 using UnityEngine.AI;
+using CodeRebirth.src.Util.Timer;
+using System;
 
 namespace CodeRebirth.src.Content.Items;
 [RequireComponent(typeof(SmartAgentNavigator))]
@@ -23,20 +25,31 @@ public class OrbitalStrike : GrabbableObject
     public SmartAgentNavigator SmartAgentNavigator { get; private set; }
     [field: SerializeField]
     public Rigidbody Rigidbody { get; private set; }
+    [field: SerializeField]
+    public float MinimumActivationTime { get; private set; }
+    [field: SerializeField]
+    public float LaserDuration { get; private set; }
+
+
+    private Timer MinimumActivationTimer;
+    private Timer LaserDurationTimer;
 
     private static readonly int RunSpeedAnimation = Animator.StringToHash("RunSpeed"); // Normalized Float 0.0-1.0
     private static readonly int DeactivateAnimation = Animator.StringToHash("Deactivate"); // Trigger
     private static readonly int ActivateAnimation = Animator.StringToHash("Activate"); // Trigger
 
 
-
-
     public override void Start()
     {
         base.Start();
+        MinimumActivationTimer = new Timer(MinimumActivationTime, TimerExecutionTime.Update);
+        LaserDurationTimer = new Timer(LaserDuration, TimerExecutionTime.Update);
+        // MinimumActivationTimer.OnFinish += ActivateLaserFromTimer;
+        LaserDurationTimer.OnFinish += DeactivateLaser;
     }
 
-    public override void ItemActivate(bool used, bool buttonDown = true) {
+    public override void ItemActivate(bool used, bool buttonDown = true)
+    {
         // public void ItemActivate(bool used, bool buttonDown = true){
         base.ItemActivate(used, buttonDown);
         if (base.IsOwner)
@@ -44,12 +57,25 @@ public class OrbitalStrike : GrabbableObject
             //Throw it
             Rigidbody.isKinematic = false;
             grabbable = false;
-            Rigidbody.AddForce(playerHeldBy.gameplayCamera.transform.forward * 30.0f,ForceMode.VelocityChange);
+            MinimumActivationTimer.Start();
+            Rigidbody.AddForce(playerHeldBy.gameplayCamera.transform.forward * 30.0f, ForceMode.VelocityChange);
             playerHeldBy.DiscardHeldObject(placeObject: true, null, playerHeldBy.transform.position);
         }
     }
 
-    private void activateLaser(){
+
+    private void DeactivateLaser(Timer t){
+        grabbable = true;
+        Animator.SetTrigger(DeactivateAnimation);
+        NavMeshAgent.enabled = false;
+        Rigidbody.isKinematic = false;
+        SmartAgentNavigator.enabled = false;
+
+    }
+
+    private void ActivateLaser() {
+        MinimumActivationTimer.Stop();
+        LaserDurationTimer.Start();
         Animator.SetTrigger(ActivateAnimation);
         NavMeshAgent.enabled = true;
         Rigidbody.isKinematic = true;
@@ -58,15 +84,19 @@ public class OrbitalStrike : GrabbableObject
         NavMeshAgent.Warp(playerHeldBy.transform.position);
     }
 
-    public override void DiscardItem()
-    {
+    public override void DiscardItem(){
         // public void DiscardItem(){
         base.DiscardItem();
     }
 
-    public override void PocketItem(){
-    // public void PocketItem(){
+    public override void PocketItem()
+    {
+        // public void PocketItem(){
         base.PocketItem();
+    }
+    public override void Update(){
+        base.Update();
+        Animator.SetFloat(RunSpeedAnimation, NavMeshAgent.velocity.magnitude / NavMeshAgent.speed);
     }
 
     public override void LateUpdate(){
@@ -74,8 +104,8 @@ public class OrbitalStrike : GrabbableObject
         if (!NavMeshAgent.enabled && Rigidbody.isKinematic){
             base.LateUpdate();
         }else if (!Rigidbody.isKinematic){
-            if(Rigidbody.velocity.magnitude<2.0){
-                activateLaser();
+            if(Rigidbody.velocity.magnitude<2.0 && MinimumActivationTimer.IsFinished){
+                ActivateLaser();
             }
         }
     }
