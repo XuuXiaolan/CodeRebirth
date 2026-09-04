11c2eb5a322e4aa0817dbd17c3b07da005c9ad66
Xu Xiaolan <ammaroaladel@gmail.com>
2026-09-04 22:55:05 +0100

    Update DuckyTube.cs

--- Plugin/CodeRebirth/src/Content/Items/DuckyTube.cs
+++ Plugin/CodeRebirth/src/Content/Items/DuckyTube.cs
@@ -1,7 +1,24 @@
+using UnityEngine;
+
 namespace CodeRebirth.src.Content.Items;
 
 public class DuckyTube : GrabbableObject
 {
+    public override void Update()
+    {
+        base.Update();
+        if (playerHeldBy == null)
+        {
+            return;
+        }
+
+        if (playerHeldBy.isUnderwater)
+        {
+            playerHeldBy.externalForceAutoFade += new Vector3(0f, Time.deltaTime, 0f);
+            playerHeldBy.ResetFallGravity();
+        }
+    }
+    
     public override void EquipItem()
     {
         base.EquipItem();
